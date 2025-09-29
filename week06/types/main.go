3ac4af1544a035e9da994e59383cc2667a579adb
MochaRhyme <tvasuper@gmail.com>
2025-09-29 14:57:51 +0900

    변수의 선언과 할당

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,9 +6,25 @@ import (
 )
 
 func main() {
-	fmt.Println(reflect.TypeOf(2.31))
-	fmt.Println(reflect.TypeOf("go developer~"))
-	fmt.Println(reflect.TypeOf('A'))
-	fmt.Println(reflect.TypeOf(true))
-	fmt.Println(reflect.TypeOf(91))
+	//선언 그리고 할당
+	//var name string
+	//var id int
+	//name="Kim Inha"
+	//id=1000
+
+	//선언과 할당
+	//var name="Kim Inha"
+	//var id=1000
+
+	//선언과 할당 (가장 짧은)
+	//이녀석도 엄연한 선언이므로, 두번 선언시 에러남.
+	name := "Kim Inha"
+	id := 1000
+
+	//평점은 소숫점 둘째짜리까지 잘리므로, float64(double)보단 float32
+	var gpa float32=3.99
+
+	fmt.Println(name, reflect.TypeOf(name))
+	fmt.Println(id, reflect.TypeOf(id))
+	fmt.Println(gpa,reflect.TypeOf(gpa))
 }
