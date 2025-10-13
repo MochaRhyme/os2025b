9a896b13b11afe1a28c8b49830c64698302d3f86
MochaRhyme <tvasuper@gmail.com>
2025-10-13 15:24:24 +0900

    loops/main.go v0.1 strconv package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	// "reflect"
 )
 
@@ -26,11 +28,39 @@ func main() {
 	// changed := changer.Replace(univ)
 	// fmt.Println(changed)
 
-	r := bufio.NewReader(os.Stdin)
-	i, err := r.ReadString('\n')
-	// fmt.Println(err)
+	// r := bufio.NewReader(os.Stdin)
+	// i, err := r.ReadString('\n')
+	// // fmt.Println(err)
+	// if err != nil {
+	// 	log.Fatal(err) //report the error, exit the program
+	// }
+	// fmt.Println(i)
+
+	//엉망진창 변수선언 - 무조건 에러!
+	// var int int = 99
+	// var b int = 8
+	// var fmt string="inha"
+	// fmt.Println(int,b)
+	// fmt.Println()
+
+	fmt.Print("Enter a score: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
 	if err != nil {
-		log.Fatal(err) //report the error, exit the program
+		log.Fatal(err)
 	}
-	fmt.Println(i)
+
+	input = strings.TrimSpace(input)            // 문자열 주위 띄어쓰기 탭키 등등 제거
+	grade, err := strconv.ParseFloat(input, 64) // 정리된 문자열을 실수타입으로 변환
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	fmt.Println("A grade of", grade, "is", status)
 }
