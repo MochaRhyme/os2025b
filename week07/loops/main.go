10d66076ace3ece5abacf09e23f9c59641bfdfec
MochaRhyme <tvasuper@gmail.com>
2025-10-13 14:21:46 +0900

    loops/main.go v0.1 time & strings package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"strings"
+	// "reflect"
+	"time"
 )
 
 func main() {
-	var length float64 = 1.2
-	var width int = 2
-	fmt.Println("면적은", int(length)*width)
-	fmt.Println("length > width?", int(length) > width)
-	fmt.Println("형변환", reflect.TypeOf(int(length)))
-	fmt.Println("원본", reflect.TypeOf(length))
+	// var length float64 = 1.2
+	// var width int = 2
+	// fmt.Println("면적은", int(length)*width)
+	// fmt.Println("length > width?", int(length) > width)
+	// fmt.Println("형변환", reflect.TypeOf(int(length)))
+	// fmt.Println("원본", reflect.TypeOf(length))
+
+	var now time.Time = time.Now()
+	// var year int = now.Year()
+	var month time.Month = now.Month()
+	var day int = now.Day()
+	fmt.Println(month, day)
+	univ := "Go$ Inha$"
+	changer := strings.NewReplacer("$", "!")
+	changed := changer.Replace(univ)
+	fmt.Println(changed)
 }
