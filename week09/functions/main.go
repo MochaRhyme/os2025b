dfc47cac655ec733e0df84b41520bbd65c56c7ee
MochaRhyme <tvasuper@gmail.com>
2025-10-27 15:00:23 +0900

    functions/main.go v0.2 pointer

--- week09/functions/main.go
+++ week09/functions/main.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 )
 
-func swap(first int, second int) {
-	temp := first
-	first = second
-	second = temp
-	fmt.Println(first, second)
+func swap(first *int, second *int) {
+	temp := *first
+	*first = *second
+	*second = temp
+	fmt.Println(*first, *second)
 }
 
 func main() {
 	// fmt.Println(math.Sqrt(-144)) 허수 그딴거 없음. NaN이 출력됨.
 	a, b := 10, 20
 	fmt.Println(a, b)
-	swap(a, b)
+	swap(&a, &b) // pass by address
 	fmt.Println(a, b)
 }
