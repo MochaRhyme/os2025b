d9f62e31685ef9880ba7f6ed9ca72ee14a90ef68
MochaRhyme <tvasuper@gmail.com>
2025-10-27 15:23:08 +0900

    function v0.3 package

--- week09/functions/main.go
+++ week09/functions/main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func swapInt(first *int, second *int) {
-	temp := *first
-	*first = *second
-	*second = temp
-	fmt.Println(*first, *second)
+// func swapInt(first *int, second *int) {
+// 	temp := *first
+// 	*first = *second
+// 	*second = temp
+// 	fmt.Println(*first, *second)
+// }
+
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
 }
 
 func main() {
-	// fmt.Println(math.Sqrt(-144)) 허수 그딴거 없음. NaN이 출력됨.
-	a, b := 10, 20
-	fmt.Println(a, b)
-	swapInt(&a, &b) // pass by address
-	fmt.Println(a, b)
+	fmt.Print("점수 입력 : ")
+	score, err := GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	status := ""
+	if score >= 90 {
+		status = "합격!"
+	} else {
+		status = "불합격"
+	}
+	fmt.Printf("%.2f점은 %v\n", score, status)
 }
