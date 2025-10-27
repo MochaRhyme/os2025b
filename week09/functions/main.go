e34c64bfdd9a433c14c197a36865fa9042b56d1d
MochaRhyme <tvasuper@gmail.com>
2025-10-27 14:54:19 +0900

    functions/main.go v0.1 pointer

---
+++ week09/functions/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func swap(first int, second int) {
+	temp := first
+	first = second
+	second = temp
+	fmt.Println(first, second)
+}
+
+func main() {
+	// fmt.Println(math.Sqrt(-144)) 허수 그딴거 없음. NaN이 출력됨.
+	a, b := 10, 20
+	fmt.Println(a, b)
+	swap(a, b)
+	fmt.Println(a, b)
+}
