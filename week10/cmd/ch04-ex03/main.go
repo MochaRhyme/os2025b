0375d4576228a330783b69c82fbf4c04fa7298a0
MochaRhyme <tvasuper@gmail.com>
2025-11-03 14:34:07 +0900

    week10 package v0.3

---
+++ week10/cmd/ch04-ex03/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"week10/pkg/keyboard"
+)
+
+func main() {
+	fmt.Printf("화씨 온도 입력 : ")
+	fahrenheit, err := keyboard.GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	celsius:=(fahrenheit-32)*5/9
+	fmt.Printf("화씨 %0.2f도는 섭씨 %0.2f도 입니다\n",fahrenheit,celsius)
+}
