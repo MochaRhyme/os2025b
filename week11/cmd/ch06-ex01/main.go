3f01a875fe882e7154a0fac48938e3912f5eab66
MochaRhyme <tvasuper@gmail.com>
2025-11-10 14:28:03 +0900

    week11 slice v0.2

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var subjects []string
-	subjects = make([]string, 3)
-	subjects[0] = "Go"
-	subjects[2] = "Python"
+	//v1
+	// var subjects []string
+	// subjects = make([]string, 3)
 
+	//v2
+	// subjects := make([]string, 3)
+	// subjects[0] = "Go"
+	// subjects[2] = "Python"
+
+	//v3
+	subjects := []string{"Go", "", "Python"} // slice literal
 	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
