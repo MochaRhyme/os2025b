94b4b5da572776c6dcd1d06ec9f48d75855d443b
MochaRhyme <tvasuper@gmail.com>
2025-11-10 14:13:10 +0900

    week11 array v0.7

---
+++ week11/cmd/ch05-ex02/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/headfirstgo/datafile"
+	"log"
+)
+
+func main() {
+	weights, err := datafile.GetFloats("meatWeight.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sum:=0.0
+	for _, number := range weights {
+		sum += number
+	}
+	sampleCount := float64(len(weights))
+	fmt.Printf("평균 고기 소비량: %0.2f\n", sum/sampleCount)
+}
