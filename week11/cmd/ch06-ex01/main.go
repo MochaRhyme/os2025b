d4452b5d70ff62da2cf7c21a8ff9903e3295e6bf
MochaRhyme <tvasuper@gmail.com>
2025-11-12 13:04:09 +0900

    Automate commiting...

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -14,13 +14,15 @@ func main() {
 
 	//v3
 	subjects := [4]string{"Go", "Javascript", "Python", "Linux"} // slice literal
-	subjectSlice:=subjects[:3] // slicing
-	subjects[0]="Java"
+	subjectSlice := subjects[:3]                                 // slicing
+	subjects[0] = "Java"
+	subjectSlice = append(subjectSlice, "Go") // 길이가 원본 배열 길이를 배반하지 않은 경우 원본에 덮어씌워짐.
+	// 배반한 경우 덮어씌워지지 않고, 원본은 그대로 따로 감.
 	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
 	fmt.Println("================")
-	for i:=0;i<len(subjectSlice);i++{
+	for i := 0; i < len(subjectSlice); i++ {
 		fmt.Println(subjectSlice[i])
 	}
 }
