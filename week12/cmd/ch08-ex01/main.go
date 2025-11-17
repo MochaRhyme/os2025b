80482e60e61051d5eae8b4d6cc72707ebdd7962c
MochaRhyme <tvasuper@gmail.com>
2025-11-17 15:11:49 +0900

    week12 struct v0.2

--- week12/cmd/ch08-ex01/main.go
+++ week12/cmd/ch08-ex01/main.go
@@ -2,19 +2,23 @@ package main
 
 import "fmt"
 
-type subscriber struct {
+type subcriber struct {
 	name  string
 	price int
 }
 
-func applyPrice(s *subscriber) {
+func applyPrice(s *subcriber) {
 	s.price = 10000
 	s.name = "Park Inha"
 }
 
 func main() {
-	var s1 subscriber
+	var s1 subcriber
+	var p *subcriber = &s1
 	// s1.name = "Kim Inha"
 	applyPrice(&s1)
 	fmt.Println(s1.name, s1.price)
+	// fmt.Println(*p.price)
+	fmt.Println((*p).price)
+	fmt.Println(p.price)
 }
